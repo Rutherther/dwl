98ff13210c819603fd79acc71778083cad7be592
Rutherther <rutherther@proton.me>
2024-05-05 13:24:11 +0200

    fix: attach clients to reenabled monitor

--- dwl.c
+++ dwl.c
@@ -2723,6 +2723,7 @@ updatemons(struct wl_listener *listener, void *data)
 			wlr_session_lock_surface_v1_configure(m->lock_surface, m->m.width, m->m.height);
 		}
 
+		attachclients(m);
 		/* Calculate the effective monitor geometry to use for clients */
 		arrangelayers(m);
 		/* Don't move clients to the left output when plugging monitors */
